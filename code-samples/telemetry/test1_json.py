318f92c8df2fcc41de8c04337dba5b26b0b22ed3
Dhivya Karthikeyan <dhkarthi@cisco.com>
2019-03-09 12:37:30 +0530

    nanog75hack

---
+++ code-samples/telemetry/test1_json.py
@@ -0,0 +1,21 @@
+{
+        "path": {
+          "elem": [
+            {
+              "key": {
+                "name": "GigabitEthernet0/0/0/0"
+              },
+              "name": "interface"
+            },
+            {
+              "name": "state"
+            },
+            {
+              "name": "admin-status"
+            }
+          ]
+        },
+        "val": {
+          "stringVal": "UP"
+        }
+      }
